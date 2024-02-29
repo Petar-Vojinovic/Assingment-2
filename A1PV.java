35b4021991692081f9f15df6d29078561fd88321
Petar Vojinovic <vojinovicpetar0@gmail.com>
2024-02-29 12:21:28 +0100

    Initial Commit

---
+++ A1PV.java
@@ -0,0 +1,13 @@
+public class A1PV {
+    
+    public static void main(String[] args)
+    {
+
+
+        for ( int x = 0; x<36; x++){
+            System.out.println("Hello World");
+        }
+
+
+    }
+}
